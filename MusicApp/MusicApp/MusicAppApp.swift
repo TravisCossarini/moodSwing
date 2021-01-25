29041aeb93e53528903f748f8b0b3fb32a54a143
hannah keefe <hannah.keefe6@gmail.com>
2021-01-25 13:07:39 -0500

    Music Component

---
+++ MusicApp/MusicApp/MusicAppApp.swift
@@ -0,0 +1,23 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Hannah  Keefe  on 2021-01-17.
+//
+
+import SwiftUI
+import Firebase
+
+@main
+struct MusicAppApp: App {
+    let data = OurData()
+    init() {
+        FirebaseApp.configure()
+        data.loadAlbums()
+    }
+    var body: some Scene {
+        WindowGroup {
+            ContentView(data: data)
+        }
+    }
+}
